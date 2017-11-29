d4eea7388b3e1df37d89893ecd30165526fea7b6
Benoit Belz <benoit.belz@gmail.com>
2017-11-29 18:51:10 +0100

    [ADD] event creation on EventList component

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { EventsService } from '../services/events.service';
 import { Observable } from 'rxjs/Observable';
 import { SimpleEvent } from '../models/simple-event';
+import { FormFeedback } from '../models/form-feedback.enum';
 
 @Component({
   selector: 'app-event-list',
@@ -10,11 +11,27 @@ import { SimpleEvent } from '../models/simple-event';
 })
 export class EventListComponent implements OnInit {
 
+  public shouldShowCreationForm = false;
+
   public events: Observable<SimpleEvent[]>;
 
   constructor(private eventsService: EventsService) { }
 
   ngOnInit() {
     this.events = this.eventsService.getEventList();
+  }
+
+  public showCreationForm(): void {
+    this.shouldShowCreationForm = true;
+  }
+
+  public onCreationFormEvent(feedback: FormFeedback): void {
+    if (feedback === FormFeedback.SUCCESS) {
+      this.shouldShowCreationForm = false;
+    } else if (feedback === FormFeedback.CANCELLED) {
+      this.shouldShowCreationForm = false;
+    } else if (feedback === FormFeedback.FAILED) {
+      console.log('Something went wrong :(');
+    }
   }
 }
