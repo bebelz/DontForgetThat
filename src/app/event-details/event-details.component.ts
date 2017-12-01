a58b63f602d393b9beb3cd96f5cf519c2987257c
Benoit Belz <benoit.belz@gmail.com>
2017-12-01 17:37:59 +0100

    [ADD] task creation from event details

--- src/app/event-details/event-details.component.ts
+++ src/app/event-details/event-details.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, ParamMap } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 import { EventsService } from '../services/events.service';
 import { SimpleEvent } from '../models/simple-event';
 import { EventTask } from '../models/event-task';
+import { FormFeedback } from '../models/form-feedback.enum';
 
 @Component({
   selector: 'app-event-details',
@@ -16,13 +17,31 @@ export class EventDetailsComponent implements OnInit {
   public event: Observable<SimpleEvent>;
   public eventTasks: Observable<EventTask[]>;
 
+  public eventId: string;
+
+  public shouldShowTaskForm = false;
+
   constructor(private route: ActivatedRoute,
               private eventsService: EventsService) {
   }
 
   ngOnInit() {
-    const id = this.route.snapshot.paramMap.get('id');
-    this.event = this.eventsService.getEvent(id);
-    this.eventTasks = this.eventsService.getEventTasks(id);
+    this.eventId = this.route.snapshot.paramMap.get('id');
+    this.event = this.eventsService.getEvent(this.eventId);
+    this.eventTasks = this.eventsService.getEventTasks(this.eventId);
+  }
+
+  public showTaskForm(): void {
+    this.shouldShowTaskForm = true;
+  }
+
+  public onCreationFormEvent(feedback: FormFeedback): void {
+    if (feedback === FormFeedback.SUCCESS) {
+      this.shouldShowTaskForm = false;
+    } else if (feedback === FormFeedback.CANCELLED) {
+      this.shouldShowTaskForm = false;
+    } else if (feedback === FormFeedback.FAILED) {
+      console.log('Something went wrong :(');
+    }
   }
 }
