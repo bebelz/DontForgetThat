050b4d03fe9f9c2c193a0b85311cebcbd44b8aa1
Benoit Belz <fatalitybugs@gmail.com>
2017-12-06 18:10:35 +0100

    [ADD] Saving users that logs in

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,14 +3,37 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { EventUser } from '../models/event-user';
 
 @Injectable()
 export class AuthService {
 
-  constructor(private afAuth: AngularFireAuth) { }
+  private COLLECTION_KEY_USERS = 'users';
+
+  constructor(private afAuth: AngularFireAuth,
+              private db: AngularFirestore) { }
 
   public login(): Promise<any> {
-    return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    return this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+      .then(data => {
+        this.db
+          .collection(this.COLLECTION_KEY_USERS)
+          .doc(data.user.uid)
+          .valueChanges()
+          .subscribe(savedUser => {
+            if (!savedUser) {
+              this.db
+                .collection(this.COLLECTION_KEY_USERS)
+                .doc(data.user.uid)
+                .set(<EventUser>{
+                  id: data.user.uid,
+                  displayName: data.user.displayName,
+                  email: data.user.email,
+                });
+            }
+          });
+      });
   }
 
   public logout(): Promise<any> {
