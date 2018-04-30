ac5e93791bade44c70a8009d281d3ef1660c3d93
Benoît Belz <benoit.belz@gmail.com>
2018-04-30 19:59:26 +0200

    [ADD] events controller

---
+++ back/DontForgetThat/Controllers/EventController.cs
@@ -0,0 +1,74 @@
+using DontForgetThat.Models;
+using DontForgetThat.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DontForgetThat.Controllers
+{
+    [Route("api/events")]
+    public class EventController : ControllerBase
+    {
+        private EventsRepository _eventsRepository;
+
+        public EventController(EventsContext context)
+        {
+            _eventsRepository = new EventsRepository(context);
+        }
+
+        [HttpGet]
+        public List<Event> GetAll()
+        {
+            return _eventsRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var ev = _eventsRepository.GetById(id);
+            if(ev == null)
+            {
+                return NotFound();
+            }
+            return Ok(ev);
+        }
+
+        [HttpGet("{eventId}/items/{itemId}")]
+        public IActionResult GetItem(int eventId, int itemId)
+        {
+            var item = _eventsRepository.GetItem(eventId, itemId);
+            return Ok(item);
+        }
+
+        [HttpPost("{eventId}/items/")]
+        public IActionResult AddItem(int eventId, [FromBody] EventItem item)
+        {
+            _eventsRepository.AddItem(eventId, item);
+            return Created("", item);
+        }
+
+        [HttpDelete("{eventId}/items/{itemId}")]
+        public IActionResult RemoveItem(int eventId, int itemId)
+        {
+            _eventsRepository.RemoveItem(eventId, itemId);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]Event ev)
+        {
+            return Ok(_eventsRepository.Create(ev));
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]string value)
+        {
+            return Forbid();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _eventsRepository.Delete(id);
+        }
+    }
+}
